917061e6819deabac1bff6335ab319aee36e2ea3
MatiasdelaBandera <matiasdelabandera@gmail.com>
2021-09-03 01:01:45 -0300

    css

--- js/login.js
+++ js/login.js
@@ -2,5 +2,15 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    document.getElementById("singInButton").addEventListener("click", login)
 });
+
+function login(){
+    var email = document.getElementById('inputEmail').value;
+    var password = document.getElementById('inputPassword').value;
+    if(email != "" && password != "") {
+        window.location.href="home.html";
+    } else {
+        alert ("campos vacios")
+    }
+}
